bd7b47dc2f107767358aaa916d43c08c6561fd4c
Shubham Tiwari <36657057+shubham9871t@users.noreply.github.com>
2025-06-02 09:50:37 +0530

    Delete simple_interest.py

--- simple_interest.py
+++
@@ -1,8 +0,0 @@
-def calculate_simple_interest(principal, rate, time):
-    return (principal * rate * time) / 100
-
-# Example
-p = 1000
-r = 5
-t = 2
-print("Simple Interest is:", calculate_simple_interest(p, r, t))
