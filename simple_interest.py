07152e66af445dbe0cf6ce6a95a2e9ca5d41c370
Shubham Tiwari <36657057+shubham9871t@users.noreply.github.com>
2025-06-02 09:36:47 +0530

    Add files via upload
    Add simple interest calculation script

---
+++ simple_interest.py
@@ -0,0 +1,8 @@
+def calculate_simple_interest(principal, rate, time):
+    return (principal * rate * time) / 100
+
+# Example
+p = 1000
+r = 5
+t = 2
+print("Simple Interest is:", calculate_simple_interest(p, r, t))
